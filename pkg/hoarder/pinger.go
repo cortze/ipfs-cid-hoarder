4a8b27f37b36062ad8d1c4235ad90087a4c7d573
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-08-05 03:17:28 +0300

    Created a new file containing the CID queue

--- pkg/hoarder/pinger.go
+++ pkg/hoarder/pinger.go
@@ -2,7 +2,6 @@ package hoarder
 
 import (
 	"context"
-	"sort"
 	"sync"
 	"time"
 
@@ -42,7 +41,24 @@ type CidPinger struct {
 	pingTaskC chan *models.CidInfo
 }
 
-// NewCidPinger return a CidPinger with the given configuration
+// NewCidPinger return a CidPinger with the given configuration,creates a new:
+//	CidPinger struct {
+//		ctx context.Context
+//		sync.Mutex
+//		wg *sync.WaitGroup
+//
+//		host         *p2p.Host
+//		dbCli        *db.DBClient
+//		pingInterval time.Duration
+//		rounds       int
+//		workers      int
+//
+//		init  bool
+//		initC chan struct{}
+//
+//		cidQ      *cidQueue
+//		pingTaskC chan *models.CidInfo
+//	}
 func NewCidPinger(
 	ctx context.Context,
 	wg *sync.WaitGroup,
@@ -315,96 +331,4 @@ func (p *CidPinger) PingPRHolder(c cid.Cid, round int, pAddr peer.AddrInfo) *mod
 		active,
 		hasRecords,
 		connError)
-}
-
-// cidQueue is a simple queue of CIDs that allows rapid access to content through maps,
-// while being abel to sort the array by closer next ping time to determine which is
-// the next soonest peer to ping
-type cidQueue struct {
-	sync.RWMutex
-
-	cidMap   map[string]*models.CidInfo
-	cidArray []*models.CidInfo
-}
-
-func newCidQueue() *cidQueue {
-	return &cidQueue{
-		cidMap:   make(map[string]*models.CidInfo),
-		cidArray: make([]*models.CidInfo, 0),
-	}
-}
-
-func (q *cidQueue) isCidAlready(c string) bool {
-	q.RLock()
-	defer q.RUnlock()
-
-	_, ok := q.cidMap[c]
-	return ok
-}
-
-func (q *cidQueue) addCid(c *models.CidInfo) {
-	q.Lock()
-	defer q.Unlock()
-
-	q.cidMap[c.CID.Hash().B58String()] = c
-	q.cidArray = append(q.cidArray, c)
-
-	return
-}
-
-func (q *cidQueue) removeCid(cStr string) {
-	delete(q.cidMap, cStr)
-	// check if len of the queue is only one
-	if q.Len() == 1 {
-		q.cidArray = make([]*models.CidInfo, 0)
-		return
-	}
-	item := -1
-	for idx, c := range q.cidArray {
-		if c.CID.Hash().B58String() == cStr {
-			item = idx
-			break
-		}
-	}
-	// check if the item was found
-	if item >= 0 {
-		q.cidArray = append(q.cidArray[:item], q.cidArray[(item+1):]...)
-	}
-}
-
-func (q *cidQueue) getCid(cStr string) (*models.CidInfo, bool) {
-	q.RLock()
-	defer q.RUnlock()
-
-	c, ok := q.cidMap[cStr]
-	return c, ok
-}
-
-func (q *cidQueue) sortCidList() {
-	sort.Sort(q)
-	return
-}
-
-// Swap is part of sort.Interface.
-func (q *cidQueue) Swap(i, j int) {
-	q.Lock()
-	defer q.Unlock()
-
-	q.cidArray[i], q.cidArray[j] = q.cidArray[j], q.cidArray[i]
-}
-
-// Less is part of sort.Interface. We use c.PeerList.NextConnection as the value to sort by.
-func (q *cidQueue) Less(i, j int) bool {
-	q.RLock()
-	defer q.RUnlock()
-
-	return q.cidArray[i].NextPing.Before(q.cidArray[j].NextPing)
-}
-
-// Len is part of sort.Interface. We use the peer list to get the length of the array.
-func (q *cidQueue) Len() int {
-	q.RLock()
-	defer q.RUnlock()
-
-	return len(q.cidArray)
 }
