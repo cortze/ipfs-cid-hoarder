1dc7dc4139a2acc4ebd0d6150c6d0239091230d0
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-10-17 20:43:09 +0300

    Bug fix methods we not finishing

--- pkg/p2p/messages.go
+++ pkg/p2p/messages.go
@@ -34,25 +34,8 @@ func (ms *MessageSender) GetMsgNotifier() *Notifier {
 	return ms.msgNot
 }
 
-// SendRequest is a custom wrapper on top of the pb.MessageSender that sends a peer a message and waits for its response
-//TODO check out if this is a response message
-//Added by fotis bistas
 func (ms *MessageSender) SendRequest(ctx context.Context, p peer.ID, pmes *pb.Message) (*pb.Message, error) {
-	// Keep track of the time that takes to send the msg
-	startT := time.Now()
-	respMsg, err := ms.m.SendRequest(ctx, p, pmes)
-	t := time.Since(startT)
-
-	// compose the Notifier
-	not := &MsgNotification{
-		RemotePeer:    p,
-		QueryTime:     startT,
-		QueryDuration: t,
-		Msg:           *pmes,
-		Error:         err,
-	}
-	ms.msgNot.Notify(not)
-	return respMsg, err
+	return ms.m.SendRequest(ctx, p, pmes)
 }
 
 // SendMessage is a custom wrapper on top of the pb.MessageSender that sends a given msg to a peer and
