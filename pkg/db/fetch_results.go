55269273fd9d690545e8742107ff3a0f2240f540
cortze <mikelcortes013@gmail.com>
2023-01-09 12:50:02 +0100

    fix retrievability bug

--- pkg/db/fetch_results.go
+++ pkg/db/fetch_results.go
@@ -44,10 +44,6 @@ func (db *DBClient) addFetchResults(fetchRes *models.CidFetchResults) (err error
 	}).Trace("adding fetch_results to DB")
 
 	tot, suc, fail := fetchRes.GetSummary()
-	var isRetrievable bool
-	if suc > 0 {
-		isRetrievable = true
-	}
 
 	_, err = db.psqlPool.Exec(db.ctx, `
 	INSERT INTO fetch_results (
@@ -77,7 +73,7 @@ func (db *DBClient) addFetchResults(fetchRes *models.CidFetchResults) (err error
 		tot,
 		suc,
 		fail,
-		isRetrievable,
+		fetchRes.IsRetrievable,
 	)
 	if err != nil {
 		return errors.Wrap(err, "unable to insert fetch_results at DB ")
